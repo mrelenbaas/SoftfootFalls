af05cd65ee4b0516202f2e2eeef37a53a6d9e885
Bradley Elenbaas <mr.elenbaas@gmail.com>
2025-09-16 23:04:14 +0000

    Added g++ to install.sh file.

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 #Git Large File system (LFS)
-sudo apt-get install git-lfs
+sudo apt-get install git-lfs -y
 
 #SDL2
 sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev -y
+
+#Compiler
+sudo apt-get install g++ -y
 
 #Valgrind
 sudo apt install valgrind -y
