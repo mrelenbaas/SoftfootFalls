7acba9e682a8f93f0712ad28c02403f89a780938
Martian__Knight <mr.elenbaas@gmail.com>
2025-09-16 18:40:13 -0400

    Added git-lfs to install.sh file.

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-#Git Large File System (LFS)
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
-echo sudo apt-get update -y
-echo sudo apt-get upgrade -y
-echo sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev -y
+#Git Large File system (LFS)
+sudo apt-get install git-lfs
+
+#SDL2
+sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev -y
+
+#Valgrind
 sudo apt install valgrind -y
