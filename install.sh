3b2efea03377a558740e90a0e0e4c1704b3c3eaa
Martian__Knight <mr.elenbaas@gmail.com>
2025-09-16 13:06:52 -0400

    Added install.sh file for Linux.

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo sudo apt-get update
+echo sudo apt-get upgrade
+echo sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
+sudo apt install valgrind
+sudo apt install code
