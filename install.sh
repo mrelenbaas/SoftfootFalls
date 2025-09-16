1491e98df8b7586a2dd1532a574aa21a0d745aed
Martian__Knight <mr.elenbaas@gmail.com>
2025-09-16 15:38:02 -0400

    Re-added install.sh file

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo sudo apt-get update -y
+echo sudo apt-get upgrade -y
+echo sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev -y
+sudo apt install valgrind -y
