7b2ac4cddb8eae6b260cfbe68fc9da046e2ea93e
Martian__Knight <mr.elenbaas@gmail.com>
2025-09-17 13:45:05 -0400

    Updated the Clock::Print function to remove the prepented 'class ' string

--- SoftfootFalls/SoftfootFalls/Clock.cpp
+++ SoftfootFalls/SoftfootFalls/Clock.cpp
@@ -21,22 +21,56 @@ void Clock::Print()
 {
 	auto PrintTrimmed = [](const char* text, long long number)
 	{
-		auto PrintWithClassName = [](const char* text, long long number)
+		auto _Print = [](int size, int totalSize, int offset, const char* text, int number)
 		{
-			printf("%s: %lld\n", text, number);
-		};
-	
-		if (isdigit(text[0]))
-		{
-			int size = text[0] - '0' + 1;
+			auto PrintWithClassName = [](const char* text, long long number)
+			{
+				printf("%s: %lld\n", text, number);
+			};
+
 			char* name = new char[size];
 			memset(name, 0, size);
-			for (int i = 0, j = 1; j < size; ++i, ++j)
+			for (int i = 0, j = offset; i < size - 1 && j < totalSize; ++i, ++j)
 				name[i] = text[j];
 			PrintWithClassName(name, number);
 			delete[] name;
+		};
+
+		int offset = 0;
+		int size = 0;
+		if (isdigit(text[0]))
+		{
+			offset = 1;
+			size = text[0] - '0' + 1;
+			
+			char* name = new char[size];
+			memset(name, 0, size);
+			for (int i = 0, j = offset; i < size - 1 && j < size; ++i, ++j)
+				name[i] = text[j];
+			_Print(size, size, offset, text, number);
+			delete[] name;
 		}
-		else __Print(text, number);
+		else //PrintWithClassName(text, number);
+		{
+			offset = 0;
+			int totalSize = 0;
+			for (char* c = (char*)text; *c != '\0'; ++c)
+			{
+				++totalSize;
+				if (isspace(*c))
+				{
+					offset = totalSize;
+				}
+			}
+			size = totalSize - offset + 1;
+			
+			char* name = new char[size];
+			memset(name, 0, size);
+			for (int i = 0, j = offset; i < size - 1 && j < totalSize; ++i, ++j)
+				name[i] = text[j];
+			_Print(size, totalSize, offset, text, number);
+			delete[] name;
+		}
 	};
 	
 	PrintTrimmed(typeid(*this).name(), Now() - start);
