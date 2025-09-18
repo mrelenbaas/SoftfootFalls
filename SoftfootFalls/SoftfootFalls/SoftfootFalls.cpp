930de571b3a872846a5aa4873b67686ab43b4f60
Martian__Knight <mr.elenbaas@gmail.com>
2025-09-18 09:15:50 -0400

    01_hello_sdl working on Windows.

--- SoftfootFalls/SoftfootFalls/SoftfootFalls.cpp
+++ SoftfootFalls/SoftfootFalls/SoftfootFalls.cpp
@@ -6,11 +6,11 @@
 #include "Clock.h"
 #include "SoftfootFalls.h"
 
-#if _WIN32
 #include <SDL3/SDL.h>
 //#include <SDL3/SDL_main.h>
+
+#if _WIN32
 #elif __linux__
-#include "SDL2/SDL.h"
 #endif
 
 
@@ -20,10 +20,41 @@ const int SCREEN_HEIGHT = 480;
 int main()
 {
     Clock* clock = new Clock();
-    //SDL_Window* window = NULL;
 
-    printf("Hello World\n");
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+		window = SDL_CreateWindow("SDL Tutorial", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+		if (window == NULL)
+		{
+			SDL_Log(
+				"Window could not be created! SDL_Error: %s\n",
+				SDL_GetError());
+		}
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillSurfaceRect(screenSurface, NULL, SDL_MapSurfaceRGB(screenSurface, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
 
+			SDL_Event e;
+			bool quit = false;
+			while (quit == false)
+			{
+				while (SDL_PollEvent(&e))
+				{
+					if (e.type == SDL_EVENT_QUIT) quit = true;
+				}
+			}
+		}
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     delete clock;
     return 0;
 }
