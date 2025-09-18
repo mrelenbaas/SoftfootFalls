ce9de3895db9a26a144598a0130b2b11348e24ca
Martian__Knight <mr.elenbaas@gmail.com>
2025-09-17 20:29:47 -0400

    Added SDL3 for Windows. Configured VS project.

--- SoftfootFalls/SoftfootFalls/SoftfootFalls.cpp
+++ SoftfootFalls/SoftfootFalls/SoftfootFalls.cpp
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <iostream>
 
+
 #include "Print.h"
 #include "Clock.h"
-
 #include "SoftfootFalls.h"
 
+#if _WIN32
+#include <SDL3/SDL.h>
+//#include <SDL3/SDL_main.h>
+#elif __linux__
+#include "SDL2/SDL.h"
+#endif
+
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main()
 {
     Clock* clock = new Clock();
+    //SDL_Window* window = NULL;
 
     printf("Hello World\n");
 
