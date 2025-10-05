8bab2617a222b8c3d07bc9c53512e0a87df9fc7e
Bradley Elenbaas <mr.elenbaas@gmail.com>
2025-10-05 16:00:43 +0000

    SDL2/Ubuntu 40... is working.

--- SoftfootFalls/SoftfootFalls/SoftfootFalls.cpp
+++ SoftfootFalls/SoftfootFalls/SoftfootFalls.cpp
@@ -402,7 +402,11 @@ bool LTexture::loadFromPixels()
 	}
 	else
 	{
+#ifdef _WIN32
 		SDL_SetSurfaceColorKey(mSurfacePixels, true, SDL_MapSurfaceRGB(mSurfacePixels, 0, 0xFF, 0xFF));
+#elif __linux__
+		SDL_SetColorKey(mSurfacePixels, SDL_TRUE, SDL_MapRGB(mSurfacePixels->format, 0, 0xFF, 0xFF));
+#endif
 		mTexture = SDL_CreateTextureFromSurface(gRenderer, mSurfacePixels);
 		if (mTexture == NULL)
 		{
@@ -413,7 +417,11 @@ bool LTexture::loadFromPixels()
 			mWidth = mSurfacePixels->w;
 			mHeight = mSurfacePixels->h;
 		}
+#ifdef _WIN32
 		SDL_DestroySurface(mSurfacePixels);
+#elif __linux__
+		SDL_FreeSurface(mSurfacePixels);
+#endif
 		mSurfacePixels = NULL;
 	}
 	return mTexture != NULL;
@@ -465,7 +473,11 @@ void LTexture::free()
 	}
 	if (mSurfacePixels != NULL)
 	{
+#ifdef _WIN32
 		SDL_DestroySurface(mSurfacePixels);
+#elif __linux__
+		SDL_FreeSurface(mSurfacePixels);
+#endif
 		mSurfacePixels = NULL;
 	}
 }
@@ -545,7 +557,11 @@ Uint32 LTexture::mapRGBA(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
 	Uint32 pixel = 0;
 	if (mSurfacePixels != NULL)
 	{
+#ifdef _WIN32
 		pixel = SDL_MapSurfaceRGBA(mSurfacePixels, r, g, b, a);
+#elif __linux__
+		pixel = SDL_MapRGBA(mSurfacePixels->format, r, g, b, a);
+#endif
 	}
 	return pixel;
 }
